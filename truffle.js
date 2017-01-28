42272d2a2b1602f2ae13fdfa41227700cc1e221f
Panagiotis Tigas <ptigas@gmail.com>
2017-01-28 13:06:05 +0000

    updated truffle.js to compile the right javascript files

--- truffle.js
+++ truffle.js
@@ -2,7 +2,8 @@ module.exports = {
   build: {
     "index.html": "index.html",
     "app.js": [
-      "javascripts/app.js"
+      "javascripts/app.js",
+      "../node_modules/latlon-geohash/latlon-geohash.js"
     ],
     "app.css": [
       "stylesheets/app.css"
